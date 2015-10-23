0ad8342e69ee94d7c7b96ab999d10481543e2f20
Robin de Mug <robin@nibor.io>
2015-10-23 17:17:35 +0200

    Implemented GOTO command

--- src/game/command/GoToCommand.cpp
+++ src/game/command/GoToCommand.cpp
@@ -16,20 +16,42 @@ namespace dc {
                 model::Room *currentRoom = &parameters.player().room();
                 model::Passage *passage = nullptr;
 
+                bool _continue = true;
                 if (heading == "north") {
                     passage = parameters.player().room().north();
-                    model::Room *sideA = &passage->sideA();
-                    model::Room *sideB = &passage->sideB();
-                    parameters.player().setRoom((sideA == currentRoom) ? *sideB : *sideA);
+                    if (passage == nullptr) {
+                        std::cout << "You cannot move to the North" << std::endl;
+                        _continue = false;
+                    }
                 } else if (heading == "east") {
-
+                    passage = parameters.player().room().east();
+                    if (passage == nullptr) {
+                        std::cout << "You cannot move to the East" << std::endl;
+                        _continue = false;
+                    }
                 } else if (heading == "south") {
-
+                    passage = parameters.player().room().south();
+                    if (passage == nullptr) {
+                        std::cout << "You cannot move to the South" << std::endl;
+                        _continue = false;
+                    }
                 } else if (heading == "west") {
-
+                    passage = parameters.player().room().west();
+                    if (passage == nullptr) {
+                        std::cout << "You cannot move to the West" << std::endl;
+                        _continue = false;
+                    }
                 } else {
                     std::cout << "Invalid direction, either use north, east, south or west" << std::endl;
                 }
+
+                if (_continue) {
+                    model::Room *sideA = &passage->sideA();
+                    model::Room *sideB = &passage->sideB();
+                    parameters.player().setRoom((sideA == currentRoom) ? *sideB : *sideA);
+                    std::cout << "You went to the next Room (" << heading << ")" << std::endl;
+                }
+
                 return;
             } else {
                 std::cout << "Could not determine the way to go without direction!" << std::endl;
