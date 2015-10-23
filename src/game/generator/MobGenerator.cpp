838cb5001ee6841e6032eaafb2c61e96578a1c99
Mike Rombout <mike.rombout1@gmail.com>
2015-10-23 10:27:27 +0200

    Add random name generators to Mobs

--- src/game/generator/MobGenerator.cpp
+++ src/game/generator/MobGenerator.cpp
@@ -1,11 +1,43 @@
+#include <vector>
 #include "MobGenerator.h"
+#include "StringGenerator.h"
+
+std::vector<std::vector<std::string>> mobNames {
+        // adjective
+        std::vector<std::string> {
+                "Deluded",
+                "Intimidating",
+                "Ignored",
+                "Beautiful",
+                "Divine",
+                "Hellish"
+        },
+
+        // TODO: Names should be base monsters name read from file
+        // names
+        std::vector<std::string> {
+                "Goblin",
+                "Skeleton",
+                "Ghoul",
+                "Slime",
+                "Vampire",
+                "Ghost",
+                "Werewolf",
+                "Troll"
+        }
+};
 
 dc::model::Mob *MobGenerator::generate(unsigned int seed, unsigned int level) {
     dc::model::Mob *mob = new dc::model::Mob();
 
-    mob->setName("Deluded Goblin");
-    mob->setHealth(100 * level);
+    // TODO: Load mob from files, with base levels
+    mob->setName(StringGenerator::generateString(mobNames, seed));
+    mob->setMaxHealth(level * 2);
+    mob->setHealth(level * 2);
     mob->setLevel(level);
+    mob->setAttack(level);     // TODO: Set (base attack * level)
+    mob->setDefence(level);    // TODO: Set (base defence * level)
+    mob->setPerception(level); // TODO: Set (base perception * level)
 
     return mob;
 }
