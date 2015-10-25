1d22f35e36e8d625fa3cdb2bfc3114183ac9da1d
Mike Rombout <mike.rombout1@gmail.com>
2015-10-25 21:04:41 +0100

    Add listing of exits to LookCommand

--- src/game/command/LookCommand.cpp
+++ src/game/command/LookCommand.cpp
@@ -1,4 +1,6 @@
+#include <sstream>
 #include <iostream>
+#include <iterator>
 #include "LookCommand.h"
 #include "Game.h"
 #include "Mob.h"
@@ -20,7 +22,25 @@ namespace dc {
         std::cout << csl::color(csl::YELLOW) << room.name() << "\n";
 
         // describe room
-        std::cout << csl::color(csl::WHITE) << mPlayer.room().description();
+        std::cout << csl::color(csl::WHITE) << mPlayer.room().description() << "\n";
+
+        // list exits
+        std::cout << "You can go: ";
+
+        std::vector<std::string> exits;
+        if(room.north())
+            exits.push_back("north");
+        if(room.east())
+            exits.push_back("east");
+        if(room.south())
+            exits.push_back("south");
+        if(room.west())
+            exits.push_back("west");
+        std::ostringstream ss;
+        std::copy(exits.begin(), exits.end() - 1, std::ostream_iterator<std::string>(ss, ", "));
+        ss << exits.back();
+
+        std::cout << ss.str() << "\n";
 
         // list monsters
         if(!room.mobs().empty()) {
