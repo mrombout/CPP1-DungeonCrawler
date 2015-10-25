02b0619660f32aa845f5bbeda6a70826046e7028
Mike Rombout <mike.rombout1@gmail.com>
2015-10-25 11:34:26 +0100

    Colorize the LookCommand

--- src/game/command/LookCommand.cpp
+++ src/game/command/LookCommand.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include "LookCommand.h"
 #include "Mob.h"
+#include "util/console.h"
 
 namespace dc {
     void game::LookCommand::execute(engine::CommandParameters &parameters) const {
         dc::model::Room& room = parameters.player().room();
 
+        // name room
+        std::cout << csl::color(csl::YELLOW) << /*room.name()*/"name" << "\n";
+
         // describe room
-        std::cout << parameters.player().room().description() << "\n\n";
+        std::cout << csl::color(csl::WHITE) << /*parameters.player().room().description()*/"desc";
 
         // list monsters
         if(!room.mobs().empty()) {
-            std::cout << "In the darkness of the rooms lurk:" << "\n";
+            std::cout << "\n\n" << csl::color(csl::RED) << /*"In the darkness of the rooms lurk:"*/"d" << "\n";
             for(dc::model::Mob *mob : room.mobs()) {
                 std::cout << "- a " << mob->name() << "(" << mob->health() << "/" << mob->maxHealth() << ")" << "\n";
             }
         }
+
+        std::cout << std::endl;
     }
 }
