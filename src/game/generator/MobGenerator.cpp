6bea366f1259ec56463bafc5088f13f37cdfc6d7
Mike Rombout <mike.rombout1@gmail.com>
2015-10-30 17:05:52 +0100

    Base mob stats on level

--- src/game/generator/MobGenerator.cpp
+++ src/game/generator/MobGenerator.cpp
@@ -12,15 +12,14 @@ namespace dc {
             dc::model::Mob *mob = new dc::model::Mob();
 
 			std::unordered_map<std::string, std::string> mobProps = ServiceLocator::getInstance().resolve<dc::game::MobLoader>().getRandomMob();
-
-            // TODO: Load mob from files, with base levels
+			
 			mob->setName(mobProps["name"]);
-			mob->setMaxHealth(Number::toInt(mobProps["maxhealth"]));
-			mob->setHealth(Number::toInt(mobProps["health"]));
-			mob->setLevel(Number::toInt(mobProps["level"]));
-			mob->setAttack(Number::toInt(mobProps["attack"]));     // TODO: Set (base attack * level)
-			mob->setDefence(Number::toInt(mobProps["defence"]));    // TODO: Set (base defence * level)
-			mob->setPerception(Number::toInt(mobProps["perception"])); // TODO: Set (base perception * level)
+			mob->setMaxHealth(Number::toInt(mobProps["maxhealth"]) * level);
+			mob->setHealth(mob->maxHealth());
+			mob->setLevel(level);
+			mob->setAttack(Number::toInt(mobProps["attack"]) * level);
+			mob->setDefence(Number::toInt(mobProps["defence"]) * level);
+			mob->setPerception(Number::toInt(mobProps["perception"]) * level);
 
             return mob;
         }
