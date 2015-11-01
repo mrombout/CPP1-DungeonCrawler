b01bb820b2a5eb3d9f59937ed4495c5df8a0e680
Mike Rombout <mike.rombout1@gmail.com>
2015-11-01 11:23:45 +0100

    Remove bell from beartrap

--- src/model/BearTrap.cpp
+++ src/model/BearTrap.cpp
@@ -7,7 +7,7 @@ namespace dc {
         const std::string BearTrap::ID = "BEARTRAP";
 
         void BearTrap::onSpring(Player &player) {
-            std::cout << "\a A bear trap suddenly grabs ahold of your leg! After a while of fiddling you manage to free yourself unharmed. But your boots are slightly damaged, you are heartbroken. Your mom just got those for you.\n";
+            std::cout << "A bear trap suddenly grabs ahold of your leg! After a while of fiddling you manage to free yourself unharmed. But your boots are slightly damaged, you are heartbroken. Your mom just got those for you.\n";
             player.decreaseHealth(5);
 
             std::cout << "Your health decreased by 5." << std::endl;
